d56ac3082a90192b50d121a94514fcad09208b89
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-10 14:38:26 +0000

    Deploy varevarao/potd_6_react_canvas to github.com/varevarao/potd_6_react_canvas.git:gh-pages

---
+++ precache-manifest.fc7088d7b93afcf96b8d735a6c69e393.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b25a9f934f4721cc296568a41caa4a1",
+    "url": "/potd_6_react_canvas/index.html"
+  },
+  {
+    "revision": "4f3bdf44acf0733963b1",
+    "url": "/potd_6_react_canvas/static/css/main.1f2fdcc1.chunk.css"
+  },
+  {
+    "revision": "5fd0ce001e3a3fd3e124",
+    "url": "/potd_6_react_canvas/static/js/2.bb3b0e2a.chunk.js"
+  },
+  {
+    "revision": "4f3bdf44acf0733963b1",
+    "url": "/potd_6_react_canvas/static/js/main.32394fe4.chunk.js"
+  },
+  {
+    "revision": "44b3841a1ecd71e6eac5",
+    "url": "/potd_6_react_canvas/static/js/runtime~main.e2b902d9.js"
+  }
+]);
