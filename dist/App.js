105bbacd56f2073d81b5d506d0d8d86c14447562
Varshneya Rao <varevarao@gmail.com>
2019-08-15 11:33:29 +0530

    Added babel presets to support commonJS imports

--- dist/App.js
+++ dist/App.js
@@ -1,29 +1,77 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import CanvasFrame from '../components/CanvasFrame';
-import ComponentCanvas from '../components/ComponentCanvas';
-import '../styles/App.css';
-import initializeStore from '../store';
-import { externalsLoaded } from '../store/actions/app';
-export default class App extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.store = initializeStore();
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactRedux = require("react-redux");
+
+var _CanvasFrame = _interopRequireDefault(require("../components/CanvasFrame"));
+
+var _ComponentCanvas = _interopRequireDefault(require("../components/ComponentCanvas"));
+
+require("../styles/App.css");
+
+var _store = _interopRequireDefault(require("../store"));
+
+var _app = require("../store/actions/app");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var App =
+/*#__PURE__*/
+function (_React$PureComponent) {
+  _inherits(App, _React$PureComponent);
+
+  function App(props) {
+    var _this;
+
+    _classCallCheck(this, App);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(App).call(this, props));
+    _this.store = (0, _store.default)();
+    return _this;
   }
 
-  componentDidMount() {
-    const {
-      components
-    } = this.props;
-    this.store.dispatch(externalsLoaded(components));
-  }
+  _createClass(App, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      var components = this.props.components;
+      this.store.dispatch((0, _app.externalsLoaded)(components));
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return _react.default.createElement("div", {
+        className: "App"
+      }, _react.default.createElement(_reactRedux.Provider, {
+        store: this.store
+      }, _react.default.createElement(_CanvasFrame.default, null, _react.default.createElement(_ComponentCanvas.default, null))));
+    }
+  }]);
 
-  render() {
-    return React.createElement("div", {
-      className: "App"
-    }, React.createElement(Provider, {
-      store: this.store
-    }, React.createElement(CanvasFrame, null, React.createElement(ComponentCanvas, null))));
-  }
+  return App;
+}(_react.default.PureComponent);
 
-}
+exports.default = App;
